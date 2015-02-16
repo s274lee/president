04b2dd9cbfb2a5dfc3cf617431de7457536e76e7
unknown <rlsantos@uwaterloo.ca>
2015-02-15 23:38:15 -0500

    Testing websockets?

--- president/game.py
+++ president/game.py
@@ -4,17 +4,36 @@ Created on Feb 15, 2015
 @author: all
 '''
 import os
-from flask import Flask
-from jinja2 import Environment, PackageLoader
+from flask import Flask #For routing
+from jinja2 import Environment, PackageLoader #For templating
 
-app = Flask(__name__)
+#Initialize components
+app = Flask(__name__) 
 env = Environment(loader=PackageLoader('game', 'templates'))
 
+#Open websocket
+class EchoWebSocket(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        self.write_message(u"You said: " + message)
+
+    def on_close(self):
+        print("WebSocket closed")
+
+#Home page route
 @app.route('/')
 def hello_world():
     template = env.get_template('mytemplate.html')
     return template.render(var='var')
 
+@app.route('/websocket')
+def test_websocket():
+    socket = new EchoWebSocket();
+    return socket
+
+#Run the web application on port 5000
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
