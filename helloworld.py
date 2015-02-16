71852be52f206a1b6fbeee5667fb0405457f1531
unknown <rlsantos@uwaterloo.ca>
2015-02-15 19:50:32 -0500

    Adding route

--- helloworld.py
+++ helloworld.py
@@ -12,6 +12,10 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/sharon')
+def truth():
+    return "She's kind of the best"
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
