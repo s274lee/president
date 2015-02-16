562bf0bc6622e39fbdb3c5b5aa84933e70b710ca
unknown <rlsantos@uwaterloo.ca>
2015-02-15 22:29:11 -0500

    Referenced the old version of helloworld.py by accident. Fixed game.py now

--- president/game.py
+++ president/game.py
@@ -3,6 +3,7 @@ Created on Feb 15, 2015
 
 @author: all
 '''
+import os
 from flask import Flask
 from jinja2 import Environment, PackageLoader
 
@@ -14,5 +15,6 @@ def hello_world():
     template = env.get_template('mytemplate.html')
     return template.render(var='var')
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
